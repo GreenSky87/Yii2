53ca2c6602e8e35d2e16793f40661691b8b2ae80
GreenSky87 <doc03_87@mail.ru>
2019-02-25 21:48:33 +0600

    Revert "Revert "dz3""
    This reverts commit 19c268b7203aa7b9d6a29a28c860f5ba09eab2c2.

--- components/ActivityComponent.php
+++ components/ActivityComponent.php
@@ -1,41 +1,40 @@
 <?php
 namespace app\components;
-
-use yii\base\Component;
 use app\models\Activity;
-
+use yii\base\Component;
+use yii\web\UploadedFile;
 class ActivityComponent extends Component
 {
-    /** @var string class of activity entity */
     public $activity_class;
-
-    public function init(){
-        parent::init();
-
-        if(empty($this->activity_class)){
-            throw_new \Exception('Need attribute activity_class');
+    public function getModel($params = null)
+    {
+        $model = new $this->activity_class;
+        if ($params && is_array($params)) {
+            $model->load($params);
+        }
+        return $model;
+    }
+    /**
+     * @param $model Activity
+     */
+    public function createActivity(&$model)
+    {
+        if ($model->validate()) {
+            $model->images = UploadedFile::getInstances($model, 'images');
+            $path = $this->getPathSaveFile();
+            foreach ($model->images as $image) {
+                $name = mt_rand(0, 9999) . time() . '.' . $image->getExtension();
+                if (!$image->saveAs($path . $name)) {
+                    $model->addError('images', 'Файл не удалось переместить');
+                    return false;
+                }
+                $model->imagesNewNames[] = $name;
+            }
+            return true;
         }
     }
-
-    
-
-    /**
-     * @return Activity
-     */
-    public function getModel($params=null){
-        /** @var Activity $model  */ 
-        $model=new $this->activity_class;
-         if ($params && is_array($params)){
-             $model->load($params);
-         }
-
-         return $model;
-    }
-
-    /** @param $model
-     *  @return bool 
-     */
-    public function createActivity($model){
-        return $model->validate();
+    private function getPathSaveFile()
+    {
+        return \Yii::getAlias('@app/web/images/');
     }
 }
