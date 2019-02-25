e1ddcb0d0d238d2e203f3e51f5c1ade91e5b6978
GreenSky87 <doc03_87@mail.ru>
2019-02-25 21:47:17 +0600

    dz3
    dz3

--- models/Activity.php
+++ models/Activity.php
@@ -2,28 +2,48 @@
 
 namespace app\models;
 use yii\base\Model;
+use yii\web\UploadedFile;
+use app\models\rules\CorrectTimeRule;
+use app\models\rules\CorrectTimeStart;
+use app\models\rules\DateTodayPlusRule;
 
 class Activity extends Model
 {
-    public $title;
+    public $title; 
+    public $dateAct; 
+    public $timeStart; 
+    public $timeEnd; 
+    public $use_notification;
     public $description;
-    public $date_start;
-    public $is_blocked;
+    public $is_blocked; 
+    public $is_repeated; 
+    public $images; 
+    public $imagesNewNames; 
 
-    function rules()
+    public function rules()
     {
         return [
-            ['title', 'string', 'max' => 150, 'min'=>2],
-            [['title', 'date_start'],'required'],
-            ['is_blocked', 'boolean']
+            [['title', 'description', 'timeStart', 'timeEnd', 'dateAct'], 'required'],
+            [['is_blocked', 'is_repeated', 'use_notification'], 'boolean'],
+            [['images'], 'file', 'extensions' => 'jpg, png', 'maxFiles' => 4],
+            ['dateAct', 'date', 'format' => 'php:d-m-Y'],
+            ['timeStart', CorrectTimeStart::class], 
+            ['timeEnd', CorrectTimeRule::class], 
+            ['dateAct', DateTodayPlusRule::class], 
         ];
     }
-    function attributeLabels()
+    public function attributeLabels()
     {
         return [
-            'title'=>'Заголовок активности',
-            'description'=>'Описание',
-            'is_blocked'=>'Блокирующее'
+            'title' => 'Название',
+            'dateAct' => 'Дата',
+            'timeStart' => 'Время начала',
+            'timeEnd' => 'Время окончания',
+            'use_notification' => 'Уведомление',
+            'images' => 'Прикрепить файлы (макс. 4 шт.)',
+            'description' => 'Описание',
+            'is_blocked' => 'Блокирующее',
+            'is_repeated' => 'Повторяющееся',
         ];
     }
 
